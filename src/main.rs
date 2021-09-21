cd96dd0b134cde73aa8c3b51ab16d917feaf8a58
ishtilkind <ishtilkind@github.com>
2021-09-21 15:46:32 -0700

    added ability to read and write io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io::{self, Read, Write};
+
+const CHUNK_SIZE: usize = 16 * 1024;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = [0; CHUNK_SIZE];
+    let num_read = io::stdin().read(&mut buffer).unwrap();
+    eprintln!("num_read: {}", num_read);
+    io::stdout().write_all(&buffer[..num_read]).unwrap();
 }
