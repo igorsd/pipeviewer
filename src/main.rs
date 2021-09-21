1f518a4c4827f6ecb789de44cf3c97b271f6140e
ishtilkind <ishtilkind@github.com>
2021-09-21 16:22:41 -0700

    Added env var handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use std::env;
 use std::io::{self, Read, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() {
-    let mut buffer = [0; CHUNK_SIZE];
-    let num_read = io::stdin().read(&mut buffer).unwrap();
-    eprintln!("num_read: {}", num_read);
-    io::stdout().write_all(&buffer[..num_read]).unwrap();
+    let silent = env::var("PV_SILENT").unwrap_or(String::new()).len() > 0;
+    let mut total_bytes = 0;
+    loop {
+        let mut buffer = [0; CHUNK_SIZE];
+        let num_read = match io::stdin().read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        total_bytes += num_read;
+        io::stdout().write_all(&buffer[..num_read]).unwrap();
+    }
+    if !silent {
+        eprintln!("total_bytes: {}", total_bytes);            
+    }
 }
