ec7520f4df4bb9c295fdccb94b224cbc4d9fdd4c
ishtilkind <ishtilkind@github.com>
2021-09-21 20:36:51 -0700

    completed 2.11

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,53 @@
+use clap::{App, Arg};
 use std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, ErrorKind, Read, Result, Write};
 
-const CHUNK_SIZE: 
-usize = 16 * 1024;
+const CHUNK_SIZE: usize = 16 * 1024;
 
-fn main() {
-    if true == true {}
-    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
+fn main() -> Result<()> {
+    let matches = App::new("pipeviewer")
+        .arg(Arg::with_name("infile").help("Read from a file instead of stdin"))
+        .arg(
+            Arg::with_name("outfile")
+                .short("o")
+                .long("outfile")
+                .takes_value(true)
+                .help("Write output to a file instead of stdout"),
+        )
+        .arg(Arg::with_name("silent").short("s").long("silent"))
+        .get_matches();
+
+        let infile = matches.value_of("infile").unwrap_or_default();
+        let outfile = matches.value_of("outfile").unwrap_or_default();
+    let silent = if matches.is_present("silent") {
+        true
+    } else {
+        !env::var("PV_SILENT").unwrap_or_default().is_empty()
+    };
+    dbg!(infile, outfile, silent);
+
     let mut total_bytes = 0;
+    let mut buffer = [0; CHUNK_SIZE];
     loop {
-        let mut buffer = [0; CHUNK_SIZE];
         let num_read = match io::stdin().read(&mut buffer) {
             Ok(0) => break,
             Ok(x) => x,
             Err(_) => break,
         };
         total_bytes += num_read;
-        io::stdout().write_all(&buffer[..num_read]).unwrap();
+        if !silent {
+            eprint!("\r{}", total_bytes);
+        }
+        if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            return Err(e);
+        }
+        //buffer.clear();
     }
     if !silent {
-        eprintln!("total_bytes: {}", total_bytes);
+        eprintln!("\rtotal_bytes: {}", total_bytes);
     }
+    Ok(())
 }
